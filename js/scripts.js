fb0ea276e61d341ddd3cc0e56481237f4356f517
Rahul Rawat <96160687+RawatRahul14@users.noreply.github.com>
2024-12-14 02:19:51 +0530

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+/*!
+* Rahul Rawat - Personal v1.0.1 (https://startbootstrap.com/template-overviews/personal)
+* Copyright 2013-2023 Rahul Rawat
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-personal/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
